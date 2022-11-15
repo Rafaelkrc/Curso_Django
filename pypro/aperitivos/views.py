404af1c47be92d3e0a4808a7f7de1b0f45354f33
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-11-15 18:22:08 +0000

    Criado a lista de vídeos e a página de indice.
    close #56

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '770651906?h=511e20b27b&amp'},
+    {'slug': 'comemoracao', 'titulo': 'Comemoração', 'vimeo_id': '771135617?h=f0b1ac0ee3&amp'}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '770651906?h=511e20b27b&amp'},
-        'comemoracao': {'titulo': 'Comemoração', 'vimeo_id': '771135617?h=f0b1ac0ee3&amp'}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
