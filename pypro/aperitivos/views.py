dff49fafdb91398a0326156d1327caed5208f371
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-11-15 13:17:52 +0000

    Atualizado mais vídeos através do slug, e criado filtro default
    close #54

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '770651906?h=511e20b27b&amp'}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '770651906?h=511e20b27b&amp'},
+        'comemoracao': {'titulo': 'Comemoração', 'vimeo_id': '771135617?h=f0b1ac0ee3&amp'}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
