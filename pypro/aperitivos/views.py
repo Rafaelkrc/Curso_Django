e054aa0e11f72d9951ac048af16819a8131924be
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-11-15 12:20:23 +0000

    Criado a forma de contexto
    close #52

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '770651906?h=511e20b27b&amp'}
+    return render(request, 'aperitivos/video.html', context={'video': video})
